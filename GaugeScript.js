b14b9c233dc9f67b2e32e6064d42e460063bf66c
john jackson <johnj33@hotmail.co.uk>
2012-12-20 14:10:22 +0000

    added gauge page

---
+++ GaugeScript.js
@@ -0,0 +1,26 @@
+﻿function Initialise() {
+    try{
+        Gauge = localStorage["Gauge"];
+        var text1 = document.getElementById("text1");
+        var text2 = document.getElementById("text2");
+        var graph1 = document.getElementById("graph1");
+        var graph2 = document.getElementById("graph2");
+        var values = new Array();
+        values = Gauge.split(",");
+        var graphurl = "http://www.environment-agency.gov.uk/homeandleisure/floods/riverlevels/Controls/RiverLevels/ChartImage.jpg?Id=";
+
+        graph1.src = graphurl + values[4] + "&ChartType=Graph";
+        graph2.src = graphurl + values[4] + "&ChartType=Histogram";
+
+        
+        
+
+        
+        alert(values[4]);
+        text1.textContent = "Gauge: " + values[3];
+        text2.textContent = "Town: " + values[0] + " \n River: " + values[2];
+    }
+    catch (ex) {
+        alert(ex);
+    }
+}
--- JavaScript.js
+++ JavaScript.js
@@ -1,46 +1,79 @@
-﻿var GetCurrentLocation = function () {
+﻿var map;
+var gauges = new Array();
+var searchResult;
+var results;
+
+var GetCurrentLocation = function () {
     var suc = function (p) {
-        try
-        {
+        try {
             var mapOptions = {
                 zoom: 14,
                 center: new google.maps.LatLng(p.coords.latitude, p.coords.longitude),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
-            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-        
-
-            if (gauges != null)
-            {
-                alert(gauges.length);
-                for (var i = 0; i < gauges.length ; i++)
-                {
-                    if (gauges[i]["station"] == "Colwick")
-                        alert(gauges[i]["url"]);
-                }
-            }
+            map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);            
         }
-        catch(ex)
-        {
+
+        catch (ex) {
             alert(ex);
         }
     };
     var locFail = function () {
         alert("fail");
     };
-    navigator.geolocation.getCurrentPosition(suc, locFail)
+    try
+    {
     
-};
-var map;
-var gauges;
-function initialize() {
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        if (gauges != null) {
+            var currentGauge;
+
+            var search = new Search("Colwick");
+            gauges.forEach(search.FindGaugeByStation);
+            if (searchResult != null) {
+                alert(searchResult.town);
+                var data = "";
+                data += searchResult.town;
+                data += "," + searchResult.url;
+                data += "," + searchResult.river;
+                data += "," + searchResult.station;
+                data += "," + searchResult.graphcode;
+                data += "," + searchResult.loc;
+                localStorage["Gauge"] = data;
+                window.location = "Gauge.html";
+                
+            }
+            else
+                alert("location not found");
+
+            //search = new Search("River Trent");
+            //gauges.forEach(search.FindGaugeByRiver);
+            //if (results.length > 0) {
+
+            //    var theTable = document.getElementById('table');
+            //    for (var i = 0, tr, td; i < results.length; i++) {
+            //        tr = document.createElement('tr');
+            //        td = document.createElement('td');
+            //        td.appendChild(document.createTextNode(results[i].station));
+            //        tr.appendChild(td);
+            //        theTable.appendChild(tr);
+            //    }
+
+            //    //document.getElementById('table').appendChild(theTable);
+            //    alert(results.length);
+            //}
+        }
     }
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    catch(ex3){
+        alert(ex3);
+    }
+
+    navigator.geolocation.getCurrentPosition(suc, locFail);
+};
+
+
+
+function initialize() {
 
     var xmlhttp = new XMLHttpRequest();
 
@@ -50,7 +83,13 @@ function initialize() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             try {
                 var text = JSON.parse(xmlhttp.responseText);
-                gauges = text["gauges"];
+                gauges = new Array();
+
+                for (var i = 0 ; i < text["gauges"].length; i++) {
+                    var currentGauge = text["gauges"][i];
+                    gauges[i] = new Gauge(currentGauge["url"], currentGauge["River"], currentGauge["station"], currentGauge["Town"], currentGauge["GraphCode"],currentGauge["loc"]);
+                }
+
             }
             catch (ex2) {
                 alert(ex2);
@@ -60,12 +99,47 @@ function initialize() {
     xmlhttp.send();
 }
 
-function GetGauge(name) {
-    //var xmlDoc = new window.XMLHttpRequest()
-    //xmlDoc.open("GET", name, false)
-    //xmlDoc.send("")
-   // JSON.parse("enviroment-agency-river-levels.json")
-    //var gauges = xmlDoc.responseXML.getElementsByTagName("Gauge")
-    //alert(gauges.toString())
-   // alert(name)
+function Gauge(url, river, station, town, graphcode, loc) {
+
+    this.url = url;
+    this.river = river;
+    this.town = town;
+    this.station = station;
+    this.graphcode = graphcode;
+    this.loc = loc;
+}
+
+//functions for searching gauge array
+function Search(value) {
+    searchResult = null;
+    results = new Array();
+    this.value = value;
+
+    this.FindGaugeByTown = function (gauge) {
+        if (gauge.Town == value) {
+            searchResult = gauge;
+            alert(this.result);
+            return;
+        }
+            
+    }
+
+    this.FindGaugeByStation = function (gauge) {
+        if (gauge.station == value) {
+            searchResult = gauge;
+            return;
+        }
+            
+    }
+
+    this.FindGaugeByRiver = function (gauge) {
+        if (gauge.river == value) {
+            results[results.length] = gauge;
+        }
+    }
+
+    this.GetAllRivers = function (gauge) {
+        results[results.length] = gauge.river;
+    }
+
 }
