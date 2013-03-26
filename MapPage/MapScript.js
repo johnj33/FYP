54cb863e22ef856e9c491b128dd78c7bad60de7e
john jackson <johnj33@hotmail.co.uk>
2013-03-26 16:55:47 +0000

    use apiv3

--- MapPage/MapScript.js
+++ MapPage/MapScript.js
@@ -24,10 +24,16 @@ function Initialise() {
             try
             {
 
-                map = new GMap2(document.getElementById("map_canvas"));
+                map = new google.maps.Map(
+                    document.getElementById('map_canvas'), {
+                    center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                    zoom: 13,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                //map = new google.maps.Map(document.getElementById("map_canvas"));
 
-                map.setCenter(new GLatLng(position.coords.latitude, position.coords.longitude), 10);
-                map.setUIToDefault();
+                //map.setCenter(new GLatLng(), 10);
+                //map.setUIToDefault();
 
                 $.getJSON("../Data/environment-agency-river-levels.json", function (data) { jsonparse(data); });
 
@@ -54,17 +60,46 @@ function Initialise() {
     
     function jsonparse(data) {
         try {
+           
             alert(data);
 
             gauges = new Array();
+            var bounds = new google.maps.LatLngBounds;
             for (var i = 0; i < data["gauges"].length; i++) {
                 var currentGauge = data["gauges"][i];
 
                 gauges[i] = new Gauge(currentGauge["url"], currentGauge["River"], currentGauge["station"], currentGauge["Town"], currentGauge["GraphCode"], currentGauge["loc"]);
                 var latlong = gauges[i].loc.split("-");
-                var marker = new GMarker(new GLatLng(latlong[0], -latlong[1]));
-                marker.title = gauges[i].station +","+gauges[i].river;
-                map.addOverlay(marker);
+                //var marker = new GMarker(new GLatLng(latlong[0], -latlong[1]));
+                //marker.title = gauges[i].station + "," + gauges[i].river;
+                //marker.data = gauges[i];
+                //marker.addListener
+                //map.addListener
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(latlong[0], -latlong[1]),
+                    map: map,
+                    data:gauges[i]
+                });
+               
+                google.maps.event.addListener(marker, 'click', function () {
+                    map.setCenter(new google.maps.LatLng(pinMarker.position.lat(), pinMarker.position.lng())); 
+                    map.setZoom(18); 
+                    onItemClick(event, pinMarker); 
+                });
+               // map.addOverlay(marker);
+                map.fitBounds(bounds);
+
+            }
+            
+            // Info window trigger function 
+            function onItemClick(event, pin) { 
+                // Create content  
+                var contentString = pin.data.station + "<br /><br /><hr />Coordinate: " + pin.data.lng +"," + pin.data.lat; 
+
+                // Replace our Info Window's content and position 
+                infowindow.setContent(contentString); 
+                infowindow.setPosition(pin.position); 
+                infowindow.open(map);
          
 
             }
