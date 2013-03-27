85fd3787af495e4aaebae1b3562e87730bb5cd11
john jackson <johnj33@hotmail.co.uk>
2013-03-27 13:00:52 +0000

    add details to pins

--- MapPage/MapScript.js
+++ MapPage/MapScript.js
@@ -29,11 +29,8 @@ function Initialise() {
                     center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
                     zoom: 13,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
-                });
-                //map = new google.maps.Map(document.getElementById("map_canvas"));
+                    });
 
-                //map.setCenter(new GLatLng(), 10);
-                //map.setUIToDefault();
 
                 $.getJSON("../Data/environment-agency-river-levels.json", function (data) { jsonparse(data); });
 
@@ -58,88 +55,95 @@ function Initialise() {
     }
 }
     
-    function jsonparse(data) {
-        try {
-           
-            alert(data);
+ function jsonparse(data) {
+     try {
+        
+         alert(data);
 
-            gauges = new Array();
-            var bounds = new google.maps.LatLngBounds;
-            for (var i = 0; i < data["gauges"].length; i++) {
-                var currentGauge = data["gauges"][i];
-
-                gauges[i] = new Gauge(currentGauge["url"], currentGauge["River"], currentGauge["station"], currentGauge["Town"], currentGauge["GraphCode"], currentGauge["loc"]);
-                var latlong = gauges[i].loc.split("-");
-                //var marker = new GMarker(new GLatLng(latlong[0], -latlong[1]));
-                //marker.title = gauges[i].station + "," + gauges[i].river;
-                //marker.data = gauges[i];
-                //marker.addListener
-                //map.addListener
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(latlong[0], -latlong[1]),
-                    map: map,
-                    data:gauges[i]
-                });
-               
-                google.maps.event.addListener(marker, 'click', function () {
-                    map.setCenter(new google.maps.LatLng(pinMarker.position.lat(), pinMarker.position.lng())); 
-                    map.setZoom(18); 
-                    onItemClick(event, pinMarker); 
-                });
-               // map.addOverlay(marker);
-                map.fitBounds(bounds);
-
-            }
-            
-            // Info window trigger function 
-            function onItemClick(event, pin) { 
-                // Create content  
-                var contentString = pin.data.station + "<br /><br /><hr />Coordinate: " + pin.data.lng +"," + pin.data.lat; 
-
-                // Replace our Info Window's content and position 
-                infowindow.setContent(contentString); 
-                infowindow.setPosition(pin.position); 
-                infowindow.open(map);
+         gauges = new Array();
+       
          
+         for (var i = 0; i < data["gauges"].length; i++) {
+             var currentGauge = data["gauges"][i];
 
-            }
+             gauges[i] = new Gauge(currentGauge["url"], currentGauge["River"], currentGauge["station"], currentGauge["Town"], currentGauge["GraphCode"], currentGauge["loc"]);
+             var latlong = gauges[i].loc.split("-");
+        
+             var marker = new google.maps.Marker({
+                 position: new google.maps.LatLng(latlong[0], -latlong[1]),
+                 map: map,
+                 title: gauges[i].station,
+                 data:gauges[i]
+             });
+             //'<script> function ViewGauge(){alert("test");} </script>' +
+             
+             google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                 return function () {
 
+                     function ViewGauge() {
+                         alert("test");
+                     }
+                     var contentString = 
+                            '<div id="content">' + 
+                            '<div id="siteNotice">' +
+                            '</div>' +
+                            '<h2 id="firstHeading" class="firstHeading">'+gauges[i].station+'</h2>' +
+                            '<div id="bodyContent">' +
+                            '<p><b>'+ gauges[i].river +'</b></p>' +
+                            '<button id="ViewGaugeBtn" onclick="ViewGauge()" > View Gauge</button>' +
+                            '</div>' +
+                            '</div>';
+                     infowindow.setContent(contentString);
+                     var data = "";
+                     data += gauges[i].town;
+                     data += "," + gauges[i].url;
+                     data += "," + gauges[i].river;
+                     data += "," + gauges[i].station;
+                     data += "," + gauges[i].graphcode;
+                     data += "," + gauges[i].loc;
+                     infowindow.open(map, marker);
+                 }
+             })(marker, i));
+             //google.maps.event.addListener(marker, 'click', function () {
+             //    map.setCenter(new google.maps.LatLng(pinMarker.position.lat(), pinMarker.position.lng())); 
+             //    map.setZoom(18); 
+             //    onItemClick(event, pinMarker); 
+             //});
 
-        }
+         }
+         var infowindow = new google.maps.InfoWindow();
+         // Info window trigger function 
+        
+     }
+     catch (ex2) {
+         alert(ex2);
+     }
+ }
 
+ function addMarker(gauge) {
+     try {
+         
+         try{
 
-        catch (ex2) {
-            alert(ex2);
+             
+         }
+         catch (ex) {
+             alert(ex);
+         }
+         
+     }
+     catch (ex3) {
+         alert(ex3);
+     }
 
-        }
-    }
+ }
 
-    function addMarker(gauge) {
-        try {
-            
-            try{
+ function Gauge(url, river, station, town, graphcode, loc) {
 
-                
-            }
-            catch (ex) {
-                alert(ex);
-            }
-            
-        }
-        catch (ex3) {
-            alert(ex3);
-        }
-
-    }
-
-    function Gauge(url, river, station, town, graphcode, loc) {
-
-        this.url = url;
-        this.river = river;
-        this.town = town;
-        this.station = station;
-        this.graphcode = graphcode;
-        this.loc = loc;
-    }
-    
-
+     this.url = url;
+     this.river = river;
+     this.town = town;
+     this.station = station;
+     this.graphcode = graphcode;
+     this.loc = loc;
+ }
